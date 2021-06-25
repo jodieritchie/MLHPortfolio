d83b028c3be325838f0966763ce168953faeb894
Cloud User <centos@ip-172-31-10-80.us-east-2.compute.internal>
2021-06-25 16:36:34 +0000

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,10 @@ from flask import Flask, render_template, redirect, request
 import csv
 from werkzeug.utils import redirect
 from . import db
+from werkzeug.security import generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.db import get_db
+
 app = Flask(__name__)
 app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
 db.init_app(app)
@@ -15,6 +19,63 @@ def index():
 @app.route('/health', methods=['GET'])
 def health_endpoint():
     return '200 OK'
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a register page
+    return render_template('register.html')
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+        user = db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            return "Login Successful", 200 
+        else:
+            return error, 418
+    
+    ## TODO: Return a login page
+    return render_template('login.html')
+
+
 
 """
 @app.route('/index.html')
